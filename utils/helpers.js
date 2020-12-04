c8d945d507ddd9d7b47303d23f63a2fe436dd8e2
NeoxAzrot1 <sami.lepays@gmail.com>
2020-12-04 16:13:56 +0100

    rendu final sans timer

--- utils/helpers.js
+++ utils/helpers.js
@@ -7,18 +7,18 @@ export const positionInDropArea = x => parseInt(x / (CURRENT_STEP), 10)
 export const positionReleased = ({ moveX, moveY }) => (isInDropArea(moveY)
   ? positionInDropArea(moveX)
   : -1)
+// Fonction pour générer une nouvelle combinaison à trouver
 export const genNewPassword = () => {
   let password = []
   for(let i = 0; i < 4; i++) {
     password.push(theme.balls[getRandom(0, theme.balls.length-1)])
   }
-  return ['#48ADA2', '#F2982F', '#48ADA2', '#48ADA2']
+  return password //['#48ADA2', '#F2982F', '#48ADA2', '#48ADA2']
 }
 
+// Fonction pour générer un nombre aléatoire dans un interval fermé
 const getRandom = (min, max) => {
   min = Math.ceil(min)
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min +1)) + min
 }
-
-//['#48ADA2', '#48ADA2', '#48ADA2', '#48ADA2']
